04e9bc576f4e601a654d9862edd5bd61ed728709
Julien Bongars <julienbongars23@gmail.com>
2025-02-20 16:21:10 +0800

    feat: add line numbers to neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -25,6 +25,13 @@ return {
           require('neo-tree.command').execute { action = 'close' }
         end,
       },
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          -- vim.opt_local.relativenumber = true
+          vim.opt_local.number = true
+        end,
+      },
     },
     filesystem = {
       window = {
