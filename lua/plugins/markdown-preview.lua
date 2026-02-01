e3c77536f689d814b226f9b0431afa79a90fb7c4
Julien Bongars <julienbongars23@gmail.com>
2026-02-02 05:17:54 +0800

    chore: added install note in markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,5 +1,7 @@
 return {
   'iamcco/markdown-preview.nvim',
+  -- May need to open any markdown file and run on first install
+  -- :call mkdp#util#install()
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = { 'markdown' },
   build = function()
